eb9b79db3760db44c451d6127b22bed3e899843a
Joel Mafra <joelhenrique78@gmail.com>
2026-03-20 15:00:56 -0300

    aula 20/03

--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'base_static/global',
+    BASE_DIR / 'base_static',
     BASE_DIR / 'contact/static',
 ]
 
